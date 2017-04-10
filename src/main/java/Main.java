1e567680a1f216f07fc860e46620293e84f7d297
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-04-10 17:14:55 +0200

    add case1 and case4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.mysql.jdbc.Connection;
+
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -5,7 +8,23 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static final String DB = "jdbc:mysql://5.135.218.27:3306/lukaszKolacz?useUnicode=true&characterEncoding=UTF-8";
+    private static final String USER = "oskar";
+    private static final String USERPW = "akademiakodu";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
     public static void main(String[] args) {
+
+
+        try {
+            Class.forName(DRIVER).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         Scanner scanner = new Scanner(System.in);
 
@@ -13,41 +32,77 @@ public class Main {
             menu();
             int numb = scanner.nextInt();
 
-            switch (numb) {
-                case 1:
+            try {
+                Connection connection = (Connection) DriverManager.getConnection(DB, USER, USERPW);
+                Statement statement = connection.createStatement();
+//                System.out.println("Połączono z bazą danych");
 
-                    break;
+                switch (numb) {
+                    case 1:
 
-                case 2:
+                        System.out.println("Podaj Imię: ");
+                        String name1 = scanner.next();
 
-                    break;
+                        System.out.println("Podaj Nazwisko: ");
+                        String lastName1 = scanner.next();
+//                    Numer telefonu nie może miec spacji! Sprawdź jak to ominąć ?! REGEX?
+                        System.out.println("Podaj numer telefonu: ");
+                        String telephoneNumber1 = scanner.next();
 
-                case 3:
 
-                    break;
+//                        addUser(connection, "Łukasz","Kołacz", "643 132 646");
+                        addUser(connection, name1, lastName1, telephoneNumber1);
 
-                case 4:
+                        break;
 
-                    break;
+                    case 2:
 
-                case 5:
+                        break;
 
-                    break;
+                    case 3:
 
-                case 6:
+                        break;
 
-                    break;
+                    case 4:
 
-                case 7:
-                    System.exit(0);
-                    break;
+                        ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
+                        while (resultSet.next()) {
+                            System.out.println("------------------");
+                            System.out.println("Id: " + resultSet.getString("id"));
+                            System.out.println("Imię: " + resultSet.getString("name"));
+                            System.out.println("Nazwisko: " + resultSet.getString("lastname"));
+                            System.out.println("Numer telefonu: " + resultSet.getString("number"));
+                            System.out.println("------------------");
+                        }
+                        resultSet.close();
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    case 7:
+                        System.exit(0);
+                        break;
+
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+
         } while (true);
 
     }
 
-    private static void menu(){
+    private static void menu() {
 
+        System.out.println(" ");
         System.out.println("--------- WITAMY W BIBLIOTECE ---------");
         System.out.println("WYBIERZ DZIAŁANIE");
         System.out.println("1. Dodaj użytkownika");
@@ -58,8 +113,23 @@ public class Main {
         System.out.println("6. Wyświetl wszystkie wypożyczenia");
         System.out.println("7. Exit");
         System.out.println("---------------------------------------");
+        System.out.println(" ");
 
     }
 
+    public static void addUser(Connection connection, String name, String lastName, String telephoneNumber) throws SQLException {
+
+        String sql = "INSERT INTO user(name, lastname, number) VALUES(?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, name);
+        statement.setString(2, lastName);
+        statement.setString(3, telephoneNumber);
+
+        statement.execute();
+
+        statement.close();
+
+        System.out.println("Dodałem użytkownika!");
+    }
 
 }
